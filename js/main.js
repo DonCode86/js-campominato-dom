cc4b6cf5213e3c239ac2b7342a337e8807bd6e27
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-09 15:38:14 +0200

    creazione array

--- js/main.js
+++ js/main.js
@@ -4,11 +4,21 @@ function getRndInteger(min, max) {
 
 //genero 16 numeri casuali tra 1 e 100 (non possono ripetersi gli stessi numeri)
 const bombs = [];
-while (bombs.length < 17) {
+let randomBombs;
+while (bombs.length < 3) {
     let randomBombs = getRndInteger(1, 100)
     if (!bombs.includes(randomBombs)) {
         bombs.push(randomBombs)
     }
 }
-console.log(bombs)
-    //faccio inserire all'utente (100-16) volte un numero alla volta, sempre tra 1-100 e sempre diversi
+console.log("bombe generate dal pc, ", bombs)
+    //faccio inserire all'utente (100-16) volte un numero alla volta, sempre tra 1-100 e sempre diversi, ogni volta verifico se questo numero e' presente in bombs.
+const userNumbers = []
+let userNum;
+while (userNumbers.length < 6) {
+    userNum = Number(prompt("Inserisci un numero"))
+    if (!userNumbers.includes(userNum) && !isNaN(userNum) && userNum > 0) {
+        userNumbers.push(userNum)
+    }
+}
+console.log("numeri scelti dall'utente, ", userNumbers)
