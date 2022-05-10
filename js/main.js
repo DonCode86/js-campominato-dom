6066587e18b2e508cbef193c9079afb95916a2a8
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-10 10:14:45 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ function getRndInteger(min, max) {
 //genero 16 numeri casuali tra 1 e 100 (non possono ripetersi gli stessi numeri)
 const bombs = [];
 let randomBombs;
+let difficulty;
+let chance;
+do {
+    difficulty = Number(prompt("Scegli un livello di difficoltà 1-2-3"))
+} while (isNaN(difficulty) || difficulty < 1 || difficulty > 3)
+if (difficulty === 1) {
+    chance = 100;
+} else if (difficulty === 2) {
+    chance = 80;
+} else if (difficulty === 3) {
+    chance = 50;
+}
+
 while (bombs.length < 16) {
     let randomBombs = getRndInteger(1, 100)
     if (!bombs.includes(randomBombs)) {
@@ -17,7 +30,7 @@ console.log("bombe generate dal pc, ", bombs)
 const userNumbers = []
 let userNum;
 let validator = false;
-while (userNumbers.length < (100 - 16)) {
+while (userNumbers.length < (chance - 16)) {
     userNum = Number(prompt("Inserisci un numero"))
 
     if (bombs.includes(userNum)) {
